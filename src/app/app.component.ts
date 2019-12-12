178a18bc376153b761281e3604ff7a4606c22c6d
Andrew <45405563+Andrew-Yan91@users.noreply.github.com>
2019-12-12 16:24:51 +1300

    Celo-Practical-Challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ElementRef, Inject, PLATFORM_ID } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { isPlatformBrowser } from '@angular/common';
+import 'core-js/es7/reflect';
+import { Meta, Title } from '@angular/platform-browser';
+import { ActivatedRoute } from "@angular/router";
+import {NgForm} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+
+export class AppComponent implements OnInit {
+  isBrowser = false
+  errorMessage: string;
+  successMessage: string;
+  myForm: FormGroup;
+  divname: any = 1;
+
+ 
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId,
+    private meta: Meta,
+    private titleService: Title,
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute
+
+  ) {
+
+
+
+    if (isPlatformBrowser(this.platformId)) {
+      this.isBrowser = true
+    }
+  }
+
+  ngOnInit() {
+    if (!this.isBrowser) {
+      return;
+    }
+  }
+
+
+  // sets the displayed left item on hover
+  mouseHoverEnter(a) {
+    this.divname = a;
+    document.getElementById(a).style.display ="block";
+  }
+  mouseHoverLeave(a){
+    this.divname = a;
+    document.getElementById(a).style.display ="none";
+  }
+  offline() {
+    document.getElementsByClassName('bigpicture1').namedItem
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
